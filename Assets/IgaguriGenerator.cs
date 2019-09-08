dc65b4e8ab419f70689faa8437a735fd400f628e
naokata <snaokata@gmail.com>
2019-09-09 03:14:46 +0900

    first commit

---
+++ Assets/IgaguriGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriGenerator : MonoBehaviour
+{
+
+	public GameObject igaguriPrefab;
+	private int unitForce = 2000;
+
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			GameObject igaguri = Instantiate (igaguriPrefab) as GameObject;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			Vector3 worldDir = ray.direction;
+			igaguri.GetComponent<IgaguriController> ().Shoot (
+				worldDir.normalized * unitForce);
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# いがぐり的当てサンプル
+
+[Unityの教科書](https://www.amazon.co.jp/dp/B07TNNTTYV/) から。練習用に github で管理。
