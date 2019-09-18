34e969774214114873e280169e0cf763b6e860ec
naokata <snaokata@gmail.com>
2019-09-19 04:09:34 +0900

    controll objects

---
+++ Assets/BallController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "target")
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<ParticleSystem>().Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/IgaguriGenerator.cs
+++ Assets/IgaguriGenerator.cs
@@ -10,12 +10,15 @@ public class IgaguriGenerator : MonoBehaviour
 
 	void Update ()
 	{
+        /*
 		if (Input.GetMouseButtonDown (0)) {
 			GameObject igaguri = Instantiate (igaguriPrefab) as GameObject;
 			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 			Vector3 worldDir = ray.direction;
 			igaguri.GetComponent<IgaguriController> ().Shoot (
 				worldDir.normalized * unitForce);
+                
 		}
+        */
 	}
 }
